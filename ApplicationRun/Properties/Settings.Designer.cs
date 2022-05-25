555faedf30647656154ceb6ddae0d2a5147f5aff
all day <yepppoison@mail.ru>
2022-05-25 14:14:20 +0300

    up2 config

--- ApplicationRun/Properties/Settings.Designer.cs
+++ ApplicationRun/Properties/Settings.Designer.cs
@@ -32,59 +32,5 @@ namespace ApplicationRun.Properties {
                 return ((string)(this["graduationworkConnectionString"]));
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=ANISINA\\SQLEXPRESS;Initial Catalog=GuideDatabase;Integrated Security=" +
-            "True")]
-        public string GuideDatabaseConnectionString {
-            get {
-                return ((string)(this["GuideDatabaseConnectionString"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=REIN\\SQLEXPRESS;Initial Catalog=GuideDatabase;Integrated Security=Tru" +
-            "e")]
-        public string GuideDatabaseConnectionString1 {
-            get {
-                return ((string)(this["GuideDatabaseConnectionString1"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=HOME\\SQLEXPRESS;Initial Catalog=GuideDatabase;Integrated Security=Tru" +
-            "e")]
-        public string GuideDatabaseConnectionString2 {
-            get {
-                return ((string)(this["GuideDatabaseConnectionString2"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=HOME\\SQLEXPRESS;Initial Catalog=GuideDatabase;Integrated Security=Tru" +
-            "e")]
-        public string GuideDatabaseConnectionString3 {
-            get {
-                return ((string)(this["GuideDatabaseConnectionString3"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=HOME\\SQLEXPRESS;Initial Catalog=Аэропорт;Integrated Security=True")]
-        public string АэропортConnectionString {
-            get {
-                return ((string)(this["АэропортConnectionString"]));
-            }
-        }
     }
 }
